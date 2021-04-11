9430cebcefb54a394ff5b1bbb851df208e517939
DelvinH <delvinhuang@yahoo.com>
2021-04-10 23:28:06 -0700

    shipenvironment basic function outline

--- shipenvironment.py
+++ shipenvironment.py
@@ -10,3 +10,21 @@ from line_intersection import intersectLines
 import pymunk
 from pymunk.vec2d import Vec2d
 from pymunk.pygame_util import DrawOptions as draw
+
+class GameState:
+    def __init__(self):
+
+
+    def create_own_ship(self):
+
+
+    def create_dynamic_obstacle(self):
+
+
+    def create_static_obstacle(self):
+
+
+    def create_goal(self):
+
+
+    def step(self):
